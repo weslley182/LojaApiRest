0aaead976e86f74765753f220a9397f4b61b1cfa
Wesley Machado <weslley182@gmail.com>
2023-04-15 15:35:02 -0300

    insert user validations

--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, isNotEmpty } from 'class-validator';
+
 export class CreateUserDTO {
+    
+    @IsNotEmpty({message: 'This name is incorrect'})
     name: string;
+
+    @IsEmail(undefined, {message: 'email must be a valid email'})
     email: string;
+
+    @MinLength(6)
     password: string;
 }
